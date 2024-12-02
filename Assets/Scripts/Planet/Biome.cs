6df8882b60eb19db1067cc7a5012d5142554a0c5
3nd3r_h3o <theenderman3333@gmail.com>
2024-12-02 16:47:56 +0100

    Added predicate structure

--- Assets/Scripts/Planet/Biome.cs
+++ Assets/Scripts/Planet/Biome.cs
@@ -3,10 +3,7 @@ using UnityEngine;
 
 public class Biome
 {
-    public float altitude;
-    public float temperature;
-    public float humity;
-    public float latitude;
+    public BiomePredicate predicate;
 
     public BiomeMaterial[] biomeMaterials;
 
@@ -22,12 +19,38 @@ public class Biome
             {
                 BiomeCarver.NONE => new float[0],
                 BiomeCarver.FRAC_SIMPLEX => new float[8]
-
             };
             carver = value;
         }
     }
     public float[] carverArguments;
+}
+
+[Serializable]
+public class BiomePredicate
+{
+    public PredicateOperation predicate;
+}
+
+
+[Serializable]
+public class PredicateOperation
+{
+    public string name;
+    [Range(-1, 1)]
+    public float value;
+    public PredicateOperation predicate;
+    /*
+    TODO bool op, to allow other ranges than everything above this value.
+    one for each predicate. IT DOESN'T CHANGE APPLICATION ORDER.
+    ex : 
+        - pole biome => 1 biome.
+        - poles different => 2 biomes.
+
+
+    default => ALL(predicate_n(value_n))
+
+    */
 }
 
 public enum BiomeCarver
