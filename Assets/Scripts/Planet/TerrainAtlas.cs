2e675fedd0916cf14702bba691842ebac70f7a18
3nd3r_h3o <theenderman3333@gmail.com>
2024-12-10 18:35:52 +0100

    Compacted atlas gen

--- Assets/Scripts/Planet/TerrainAtlas.cs
+++ Assets/Scripts/Planet/TerrainAtlas.cs
@@ -27,85 +27,46 @@ public class TerrainAtlas
     {
         //TODO mouve it in CS later
         if (atlas != null) Cleanup();
+        int texSize = 256;
+
         atlas = new Texture3D[6];
-        Texture3D albedos = new Texture3D(256, 256, types.Length, TextureFormat.RGBA32, false);
+        Texture3D albedos = new Texture3D(texSize, texSize, types.Length, TextureFormat.RGBA32, false);
+        Texture3D normalMaps = new Texture3D(texSize, texSize, types.Length, TextureFormat.RGBA32, false);
+        Texture3D heights = new Texture3D(texSize, texSize, types.Length, TextureFormat.RGBA32, false);
+        Texture3D metalics = new Texture3D(texSize, texSize, types.Length, TextureFormat.RGBA32, false);
+        Texture3D roughnesss = new Texture3D(texSize, texSize, types.Length, TextureFormat.RGBA32, false);
+        Texture3D occlusions = new Texture3D(texSize, texSize, types.Length, TextureFormat.RGBA32, false);
+
         for (int z = 0; z < types.Length; z++)
         {
-            for (int y = 0; y < types[z].albedo.height; y++)
+            for (int y = 0; y < texSize; y++)
             {
-                for (int x = 0; x < types[z].albedo.width; x++)
+                for (int x = 0; x < texSize; x++)
                 {
                     albedos.SetPixel(x, y, z, types[z].albedo.GetPixel(x, y));
-                }
-            }
-        }
-        albedos.Apply();
-        atlas[0] = albedos;
-        Texture3D normalMaps = new Texture3D(256, 256, types.Length, TextureFormat.RGBA32, false);
-        for (int z = 0; z < types.Length; z++)
-        {
-            for (int y = 0; y < types[z].normalMap.height; y++)
-            {
-                for (int x = 0; x < types[z].normalMap.width; x++)
-                {
                     normalMaps.SetPixel(x, y, z, types[z].normalMap.GetPixel(x, y));
-                }
-            }
-        }
-        normalMaps.Apply();
-        atlas[1] = normalMaps;
-        Texture3D heights = new Texture3D(256, 256, types.Length, TextureFormat.RGBA32, false);
-        for (int z = 0; z < types.Length; z++)
-        {
-            for (int y = 0; y < types[z].height.height; y++)
-            {
-                for (int x = 0; x < types[z].height.width; x++)
-                {
                     heights.SetPixel(x, y, z, types[z].height.GetPixel(x, y));
-                }
-            }
-        }
-        heights.Apply();
-        atlas[2] = heights;
-        Texture3D metalics = new Texture3D(256, 256, types.Length, TextureFormat.RGBA32, false);
-        for (int z = 0; z < types.Length; z++)
-        {
-            for (int y = 0; y < types[z].metalic.height; y++)
-            {
-                for (int x = 0; x < types[z].metalic.width; x++)
-                {
                     metalics.SetPixel(x, y, z, types[z].metalic.GetPixel(x, y));
-                }
-            }
-        }
-        metalics.Apply();
-        atlas[3] = metalics;
-        Texture3D roughnesss = new Texture3D(256, 256, types.Length, TextureFormat.RGBA32, false);
-        for (int z = 0; z < types.Length; z++)
-        {
-            for (int y = 0; y < types[z].roughness.height; y++)
-            {
-                for (int x = 0; x < types[z].roughness.width; x++)
-                {
                     roughnesss.SetPixel(x, y, z, types[z].roughness.GetPixel(x, y));
-                }
-            }
-        }
-        roughnesss.Apply();
-        atlas[4] = roughnesss;
-        Texture3D occlusions = new Texture3D(256, 256, types.Length, TextureFormat.RGBA32, false);
-        for (int z = 0; z < types.Length; z++)
-        {
-            for (int y = 0; y < types[z].occlusion.height; y++)
-            {
-                for (int x = 0; x < types[z].occlusion.width; x++)
-                {
                     occlusions.SetPixel(x, y, z, types[z].occlusion.GetPixel(x, y));
                 }
             }
         }
-        occlusions.Apply();
+        
+        atlas[0] = albedos;
+        atlas[1] = normalMaps;
+        atlas[2] = heights;
+        atlas[3] = metalics;
+        atlas[4] = roughnesss;
         atlas[5] = occlusions;
+
+        
+        albedos.Apply();
+        normalMaps.Apply();
+        heights.Apply();
+        metalics.Apply();
+        roughnesss.Apply();
+        occlusions.Apply();
     }
 }
 
