2d2cfd334e9d92aec5c297375ef1acbd584aa0e9
3nd3r_h3o <theenderman3333@gmail.com>
2024-12-24 17:53:49 +0100

    started physic system template

---
+++ Assets/Physics-test/NewtonPhysicController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class NewtonPhysicController : MonoBehaviour
+{
+    //TODO register planets
+    void Start()
+    {
+        
+    }
+
+    //TODO apply gravity
+    void FixedUpdate()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
